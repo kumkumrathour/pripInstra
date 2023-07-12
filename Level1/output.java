34b01902b9d6b83eaac4541a20a51391b8fdb2a9
kumkumrathour <neharathour9669535194@gmail.com>
2023-07-12 22:41:48 +0530

    first commit

---
+++ Level1/output.java
@@ -0,0 +1,10 @@
+package Level1;
+import java.util.*;
+public class output {
+    public static void main(String[] args){
+        // System.out.println("*");
+        // System.out.println("* " + "*");
+        // System.out.println("* " + "* " + "* ");
+        System.out.println("* " + "\n" + "* " +"* " + "\n" + "* "+ "* " + "* ");
+    }
+}
